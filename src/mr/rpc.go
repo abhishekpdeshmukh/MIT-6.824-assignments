ff63cda9448f3d0f1e85b7461eabd5826224bf71
abhishekpdeshmukh <38159363+abhishekpdeshmukh@users.noreply.github.com>
2024-07-06 12:23:10 -0400

    Added RPC structure

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,8 +6,10 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
-import "strconv"
+import (
+	"os"
+	"strconv"
+)
 
 //
 // example to show how to declare the arguments
@@ -15,15 +17,22 @@ import "strconv"
 //
 
 type ExampleArgs struct {
-	X int
+	File string
 }
 
 type ExampleReply struct {
-	Y int
+	File string
+}
+
+type TaskOrderReply struct {
+	Type            string
+	File            string
+	TaskNum         int
+	currMapIndex    int
+	currReduceIndex int
 }
 
 // Add your RPC definitions here.
-
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
